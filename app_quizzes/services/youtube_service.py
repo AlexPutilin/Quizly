b541a3e7702f14c6debcd84afff4984433ca89a2
Alexander Putilin <alexander.putilin@yahoo.de>
2026-06-04 08:58:51 +0200

    refactor: remove extract_video_info function and clean up download_youtube_audio

--- app_quizzes/services/youtube_service.py
+++ app_quizzes/services/youtube_service.py
@@ -21,20 +21,3 @@ def download_youtube_audio(video_url, output_dir):
     with yt_dlp.YoutubeDL(options) as yt_downloader:
         yt_downloader.download([video_url])
     return str(Path(output_dir) / 'audio.mp3')
-
-
-def extract_video_info(video_url):
-    """Extracts and returns the YouTube video information"""
-
-    options = {
-        'quiet': True,
-        'no_warnings': True,
-    }
-
-    with yt_dlp.YoutubeDL(options) as yt_downloader:
-        info = yt_downloader.extract_info(video_url, download=False)
-        return {
-            'title': info.get('title'),
-            'duration': info.get('duration'),
-            'description': info.get('description')
-        }
