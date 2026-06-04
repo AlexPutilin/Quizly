b2c3fa463201299ab8a1d556fc9b1a70a4381ab4
Alexander Putilin <alexander.putilin@yahoo.de>
2026-06-04 10:31:26 +0200

    feat: implement admin configuration for Quiz and Question models

--- app_quizzes/admin.py
+++ app_quizzes/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from models import Quiz, Question
 
-# Register your models here.
+
+class QuizAdmin(admin.ModelAdmin):
+    """Admin config for quizzes"""
+
+    list_display = ('id', 'title', 'user', 'created_at')
+    list_filter = ('created_at', 'updated_at')
+    search_fields = ('user', 'title', 'description', 'video_url')
+    readonly_fields = ('created_at', 'updated_at')
+    ordering = ('-created_at')
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    """Admin config for questions"""
+
+    list_display = ('id', 'quiz', 'question_title', 'answer')
+    list_filter = ('quiz')
+    search_fields = ('question_title', 'answer')
+    readonly_fields = ('created_at', 'updated_at')
+    ordering = ('quiz', 'id')
+
+
+admin.site.register(Quiz, QuizAdmin)
+admin.site.register(Question, QuestionAdmin)
